b6145ded54e54a01ac7647267dd0b3e2758afd7d
Gmelchioretto <72448443+Gmelchioretto@users.noreply.github.com>
2020-10-28 16:31:32 +0100

    Update HelloMoi.java

--- HelloMoi.java
+++ HelloMoi.java
@@ -1,5 +1,5 @@
 public class HelloMoi {
-public static void main(string[] args) {
-system.out.println("hello, word!");
+public static void main(String[] args) {
+System.out.println("hello, word!");
 }
 }
